42f1efdad279e1184a1ca182ce754e950cc9adb2
mecottl <126541744+mecottl@users.noreply.github.com>
2023-10-04 08:48:51 -0600

    password.py

--- password.py
+++ password.py
@@ -1,13 +1,13 @@
 #Reto1 crea una contraseña aleatoria
 import random
 
-char = "abcdefghijklmnopqrstuvwxyz1234567890"
-password = ""
-dig = 0
+char = "abcdefghijklmnopqrstuvwxyz1234567890" #Banco de caracteres disponibles
+password = "" #Variable donde se guarda la password
+dig = 0 #Variable para la cantidad de digitos que vendrán
 
-dig = int(input("Cuantos caracteres debe tener tu password?"))
+dig = int(input("Cuantos caracteres debe tener tu password?")) #input para los caracteres a ingresar 
 
 for i in range(dig):
-    password += random.choice(char)
+    password += random.choice(char) #ingresar los datos de forma aleatoria elegiendo '.choice' alguno del banco de caracteres '(char)'
     
-print("Tu password aleatorio de", dig, " caracteres es: ", password)
+print("Tu password aleatorio de", dig, " caracteres es: ", password) #Imprimir la contraseña generada
